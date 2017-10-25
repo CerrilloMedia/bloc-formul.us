b6ad324b2982798bb155b87266e15c89bb92b333
CMDS <beskar_iv@CMDSs-MacBook-Air.local>
2017-10-25 12:18:10 -0700

    refactor views & partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,13 +49,14 @@ class UsersController < ApplicationController
                   end
                 when current_user.client? && @user.artist?
                   # when a client visists an artists page, should only return their own client history with specific artist.
-                  @user.guest_formulas.where('client_id = ?', current_user.id)
-                when current_user.artist? && @user.client?
+                  @user.guest_formulas & current_user.formulas
+                when current_user.artist? && ( @user.client? || @user.artist? )
                   # when artist visits any client page it shows clients entire general history
-                  @user.formulas.first(10)
-                when current_user.artist? && @user.artist?
+                #   @user.formulas.first(10)
+                # when current_user.artist? && @user.artist?
                   # when user as artist visits another artists page, it shows the users guest formulas
-                  @user.guest_formulas.first(10)
+                  # current_user.guest_formulas.where(client_id: @user.id)
+                  @user.formulas.first(10)
                 when current_user.client? && @user.client?
                   []
                 end
